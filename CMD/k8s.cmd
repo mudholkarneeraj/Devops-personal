2368dcf94671406bb0caafe555d38071d009e1c8
Neerajkumar171 <125264937+Neerajkumar171@users.noreply.github.com>
2024-10-03 17:26:55 +0530

    Update and rename k8s.md to k8s.cmd

--- CMD/k8s.cmd
+++ CMD/k8s.cmd
@@ -3,3 +3,4 @@
 3. kubectl --namespace monitoring port-forward $POD_NAME 3000
 4. 
 $ kubectl -n kube-system run my-shell --rm -i --tty --image ubuntu:latest -- /bin/bash
+$ kubectl run -n staging -i --tty load-generator-3 --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://api-service:5000/healthCheck; done"
